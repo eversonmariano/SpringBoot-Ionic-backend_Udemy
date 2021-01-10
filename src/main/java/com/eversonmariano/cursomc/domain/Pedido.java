65739f5c1663f84c29164e0e01a8bcfb2c5d679e
eversonmariano <mariano.computacao@gmail.com>
2021-01-10 12:35:56 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/eversonmariano/cursomc/domain/Pedido.java
+++ src/main/java/com/eversonmariano/cursomc/domain/Pedido.java
@@ -1,17 +1,11 @@
 package com.eversonmariano.cursomc.domain;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import java.util.Set;
 
 @Entity
 public class Pedido implements Serializable {
@@ -32,6 +26,17 @@ public class Pedido implements Serializable {
     @ManyToOne
     @JoinColumn(name="endereco_de_entrega_id")
     private Endereco enderecoDeEntrega;
+
+    @OneToMany(mappedBy = "id.pedido")
+    private Set<ItemPedido> itens = new HashSet<>();
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
+    }
 
     public Pedido() {
     }
